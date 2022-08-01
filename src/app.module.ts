56563e60729c9b3b2c6b2323c02e3f477afb7014
bohdanyakymchuk <yakybo@ukr.net>
2022-08-01 11:31:31 +0300

    New nest-js test project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,21 @@ import { AppService } from './services/app.service';
 import { AppRepository } from './repositories/app.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Note } from './entities/note.entity';
+import { AuthService } from './services/auth.service';
+import { User } from './entities/user.entity';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [Note],
+      entities: [Note, User],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Note]),
+    TypeOrmModule.forFeature([Note, User]),
   ],
-  controllers: [AppController],
-  providers: [AppService, AppRepository],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AppRepository, AuthService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['qwerty'],
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(8888);
 }
+
 bootstrap();
